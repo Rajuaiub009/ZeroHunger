b5c8c5ed189338a2dc59ce76001d318534ac1cfa
Raju Dey <raju.dey.aiub@gmail.com>
2023-03-21 19:22:03 +0600

    Raju Dey

---
+++ MidAssignment/Controllers/RegController.cs
@@ -0,0 +1,149 @@
+﻿using MidAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidAssignment.Controllers
+{
+    public class RegController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RegIndex()
+        {
+           
+            var db = new ZeroHungerEntities();
+            var users = db.Users.ToList();
+            return View(users);
+        }
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(User user)
+        {
+            var db = new ZeroHungerEntities();
+            db.Users.Add(user);
+            db.SaveChanges();
+            TempData["msg"] = "Sucessfully Added";
+            return RedirectToAction("RegIndex");
+        }
+
+        [HttpGet]
+        public ActionResult AdminDashboard()
+        {
+            var db = new ZeroHungerEntities();
+            var orders = db.Orders.ToList();
+            return View(orders);
+        }
+
+        [HttpGet]
+        public ActionResult EmpAdmin()
+        {
+            var db = new ZeroHungerEntities();
+            var users = db.Users.ToList();
+            return View(users);
+        }
+        [HttpGet]
+        public ActionResult Order()
+        {
+           
+            var db = new ZeroHungerEntities();
+            var orders = db.Orders.ToList();
+            TempData["msg"] = "Successfully Added";
+            return View(orders);
+        }
+
+        [HttpGet]
+        public ActionResult ResAdmin()
+        {
+            var db = new ZeroHungerEntities();
+            var users = db.Users.ToList();
+            return View(users);
+        }
+      
+
+        [HttpGet]
+        public ActionResult RestaurantDashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RestaurantDetails()
+        {
+            var db = new ZeroHungerEntities();
+            var users = db.Users.ToList();
+            return View(users);
+        }
+
+        public ActionResult EmployeeDashboard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RestaurantDashboard(Order order)
+        {
+            var db = new ZeroHungerEntities();
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return RedirectToAction("Order");
+        }
+
+        [HttpGet]
+        public ActionResult AdminHistory()
+        {
+            var db = new ZeroHungerEntities();
+            var orders = db.Orders.ToList();
+            return View(orders);
+        }
+        [HttpGet]
+        public ActionResult Login() {
+            return View();
+           }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            TempData["msg"] = "Logged in successfully.";
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new ZeroHungerEntities())
+                {
+                    var obj = db.Users.Where(a => a.email.Equals(user.email) && a.password.Equals(user.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        
+                        if (obj.type == "Admin")
+                        {
+                            return RedirectToAction("AdminDashBoard");
+                        }
+                        else if (obj.type == "Restaurant")
+                        {
+                            return RedirectToAction("RestaurantDashBoard");
+                        }
+                        else
+                        {
+                            return RedirectToAction("EmployeeDashBoard");
+                        }
+
+                    }
+                }
+            }
+            return View(user);
+        }
+    }
+}
